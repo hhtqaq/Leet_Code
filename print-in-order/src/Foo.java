a24509b5a9cfe6213ff0dbc4e78bdca55d4f526a
huanghaitao <A10696@tvt.net.cn>
2019-09-12 18:07:26 +0800

    顺序打印

---
+++ print-in-order/src/Foo.java
@@ -0,0 +1,59 @@
+/**
+ * 我们提供了一个类：
+ * <p>
+ * public class Foo {
+ *   public void one() { print("one"); }
+ *   public void two() { print("two"); }
+ *   public void three() { print("three"); }
+ * }
+ * 三个不同的线程将会共用一个 Foo 实例。
+ * <p>
+ * 线程 A 将会调用 one() 方法
+ * 线程 B 将会调用 two() 方法
+ * 线程 C 将会调用 three() 方法
+ * 请设计修改程序，以确保 two() 方法在 one() 方法之后被执行，three() 方法在 two() 方法之后被执行
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/print-in-order
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: "onetwothree"
+ * 解释:
+ * 有三个线程会被异步启动。
+ * 输入 [1,2,3] 表示线程 A 将会调用 one() 方法，线程 B 将会调用 two() 方法，线程 C 将会调用 three() 方法。
+ * 正确的输出是 "onetwothree"。
+ */
+class Foo {
+
+    public Foo() {
+
+    }
+
+    class PrintTask implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
